2352d9590d298075fa41cab3fadc2af4a1d76a0e
TechnologicalJerry <cserajatverma@gmail.com>
2026-01-05 22:48:51 +0530

    Add Capacitor configuration file
    Introduces capacitor.config.ts with basic app settings for Capacitor integration, including appId, appName, and webDir.

---
+++ angular-ionic-client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'online-payment-system',
+  webDir: 'www'
+};
+
+export default config;
